69bb0d2661dd3cf4e03463aee0eb312601f7d7ad
narichasavanorkejoyce <nari.savanorkejoyce@gmail.com>
2017-05-17 10:20:02 -0400

    Adds user authentication code
    Added authentication code using handlebar partials. User will have ability
    to sign-up, sign-in, change password, and sign-out.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -3,10 +3,16 @@
 const setAPIOrigin = require('../../lib/set-api-origin')
 const config = require('./config')
 const events = require('./uploads/events.js')
+const authEvents = require('./auth/events.js')
 
 $(() => {
   setAPIOrigin(location, config)
   events.onShowAllUploads()
+})
+
+// Bring in login events
+$(() => {
+  authEvents.addHandlers()
 })
 
 // use require with a reference to bundle the file and use it in this file
