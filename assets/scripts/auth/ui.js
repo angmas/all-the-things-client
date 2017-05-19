b253fcfd33b44aeb86d96f1408baaf765cac70e5
aeabbott <amandaeabbott@gmail.com>
2017-05-18 22:58:12 -0400

    Removes console.logs from auth ui file

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -2,30 +2,30 @@
 const store = require('../store.js')
 
 const successSignUp = (data) => {
-  console.log(data)
+  // console.log(data)
   $('#sign-up-response').text('Awesome! Now, please sign in.')
 }
 
 const failureSignUp = (error) => {
-  console.error(error)
+  // console.error(error)
   $('#sign-up-response').text('User already exists. Please sign in.')
 }
 
 const signInSuccess = (data) => {
   store.user = data.user
-  console.log(store)
+  // console.log(store)
   $('#sign-in-response').text('Success! User has signed in.')
   return
 }
 
 const signInFail = (error) => {
-  console.error(error)
+  // console.error(error)
   $('#sign-in-response').text('Wrong password. Please try again.')
 }
 
 const signOutSuccess = () => {
   store.user = null
-  console.log(store)
+  // console.log(store)
   $('#sign-out-response').text('User has signed out!')
 }
 
@@ -33,18 +33,18 @@ const changePasswordSuccess = () => {
   $('.old-password-mismatch-message').hide()
   $('.pass-success-message').show()
   $('#change-password')[0].reset()
-  console.log('Password Successfully Changed.')
+  // console.log('Password Successfully Changed.')
 }
 
 const changePasswordFail = (error) => {
-  console.error('change pass', error)
+  // console.error('change pass', error)
   $('#change-password')[0].reset()
   $('.old-password-mismatch-message').show()
   $('.pass-success-message').hide()
 }
 
 const failure = (error) => {
-  console.error(error)
+  // console.error(error)
 }
 
 module.exports = {
